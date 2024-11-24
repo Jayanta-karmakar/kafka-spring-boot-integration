3eaef50388dcf87ffddcc27733965afc612fdb8a
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2024-11-24 19:53:22 +0530

    restructure

--- src/main/java/com/kafka/kafka_spring_boot_integration/config/KafkaProducerConfig.java
+++ src/main/java/com/kafka/kafka_spring_boot_integration/config/KafkaProducerConfig.java
@@ -1,11 +1,13 @@
 package com.kafka.kafka_spring_boot_integration.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -25,6 +27,12 @@ public class KafkaProducerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaServer;
+
+    @Bean
+    public NewTopic createTopic() {
+        return TopicBuilder.name("test")
+                .build();
+    }
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
--- src/main/java/com/kafka/kafka_spring_boot_integration/controller/KafkaController.java
+++ src/main/java/com/kafka/kafka_spring_boot_integration/controller/KafkaController.java
@@ -1,10 +1,10 @@
 package com.kafka.kafka_spring_boot_integration.controller;
 
-import com.kafka.kafka_spring_boot_integration.utils.MessageProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class KafkaController {
 
     @Autowired
-    public MessageProducer messageProducer;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping(value = "/send")
     public ResponseEntity<?> sendMessageToKafka(
             @RequestParam(value = "message") String message) {
         String topic = "test";
-        messageProducer.sendMessage(topic, message);
+        kafkaTemplate.send(topic, message);
         return new ResponseEntity<>("Message sent: " + message, HttpStatus.OK);
     }
 }
